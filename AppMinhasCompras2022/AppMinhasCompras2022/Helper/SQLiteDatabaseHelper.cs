a26db1c3d477f372b93a1ce11afde9a394cfef2b
BrunoozL <Brunorighi267@gmail.com>
2022-11-21 10:35:38 -0300

    Adicionar arquivos de projeto.

---
+++ AppMinhasCompras2022/AppMinhasCompras2022/Helper/SQLiteDatabaseHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AppMinhasCompras2022.Model;
+using SQLite;
+
+namespace AppMinhasCompras2022.Helper
+{
+    public class SQLiteDatabaseHelper
+    {
+        readonly SQLiteAsyncConnection _conn;
+
+        public SQLiteDatabaseHelper(string path)
+        {
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<Produto>().Wait();
+        }
+        public Task<int> Insert(Produto p)
+        {
+            return _conn.InsertAsync(p);
+        }
+
+        public void Update(Produto p)
+        {
+            string sql = "UPDATE produto SET descricao=?, quantidade=?, preco=? WHERE id=?";
+
+            _conn.QueryAsync<Produto>(sql, p.descricao, p.quantidade, p.preco, p.id);
+        }
+
+        public Task<List<Produto>> GetAll()
+        {
+            return _conn.Table<Produto>().ToListAsync();
+        }
+
+        public Task<int> Delete(int id)
+        {
+            return _conn.Table<Produto>().DeleteAsync(i => i.id == id);
+        }
+    }
+}
---
+++ AppMinhasCompras2022/AppMinhasCompras2022/View/Listagem.xaml.cs
@@ -0,0 +1,58 @@
+﻿using AppMinhasCompras2022.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppMinhasCompras2022.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Listagem : ContentPage
+    {
+        public Listagem()
+        {
+            InitializeComponent();
+        }
+
+        private void ToolbarItem_Clicked_Novo(object sender, EventArgs e)
+        {
+            try
+            {
+                Navigation.PushAsync(new NovoProduto());
+            }
+            catch(Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+
+        private void ToolbarItem_Clicked_Somar(object sender, EventArgs e)
+        {
+
+        }
+
+        protected override void OnAppearing()
+        {
+            ObservableCollection<Produto> lista_produtos = new ObservableCollection<Produto>();
+
+            System.Threading.Tasks.Task.Run(async () =>
+            {
+                List<Produto> temp = await App.Database.GetAll();
+
+                foreach (Produto item in temp)
+                {
+                    lista_produtos.Add(item);
+                }
+
+                ref_carregando.IsRefreshing = false;
+            });
+
+            lst_produtos.ItemsSource = lista_produtos;
+        }
+    }
+}
